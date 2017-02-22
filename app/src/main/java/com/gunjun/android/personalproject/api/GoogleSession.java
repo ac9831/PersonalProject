91ceb70e784cda3c4b1ef665f63d04d4b68beaa5
Lee GunJun <ac9831@ajou.ac.kr>
2017-02-22 20:03:46 +0900

    빠진 수정사항 처리 및 반영

--- app/src/main/java/com/gunjun/android/personalproject/api/GoogleSession.java
+++ app/src/main/java/com/gunjun/android/personalproject/api/GoogleSession.java
@@ -13,10 +13,10 @@ public class GoogleSession {
     private SharedPreferences.Editor editor;
 
     private static final String SHARED = "Google_Preferences";
-    private static final String API_USERNAME = "username";
-    private static final String API_ID = "id";
-    private static final String API_NAME = "name";
-    private static final String API_ACCESS_TOKEN = "access_token";
+    private static final String KEY_USERNAME = "username";
+    private static final String KEY_ID = "id";
+    private static final String KEY_NAME = "name";
+    private static final String KEY_ACCESS_TOKEN = "access_token";
 
     public GoogleSession(Context context) {
         sharedPref = context.getSharedPreferences(SHARED, Context.MODE_PRIVATE);
@@ -25,42 +25,42 @@ public class GoogleSession {
 
 
     public void storeAccessToken(String accessToken, String id, String username, String name) {
-        editor.putString(API_ID, id);
-        editor.putString(API_NAME, name);
-        editor.putString(API_ACCESS_TOKEN, accessToken);
-        editor.putString(API_USERNAME, username);
+        editor.putString(KEY_ID, id);
+        editor.putString(KEY_NAME, name);
+        editor.putString(KEY_ACCESS_TOKEN, accessToken);
+        editor.putString(KEY_USERNAME, username);
         editor.commit();
     }
 
     public void storeAccessToken(String accessToken) {
-        editor.putString(API_ACCESS_TOKEN, accessToken);
+        editor.putString(KEY_ACCESS_TOKEN, accessToken);
         editor.commit();
     }
 
 
     public void resetAccessToken() {
-        editor.putString(API_ID, null);
-        editor.putString(API_NAME, null);
-        editor.putString(API_ACCESS_TOKEN, null);
-        editor.putString(API_USERNAME, null);
+        editor.putString(KEY_ID, null);
+        editor.putString(KEY_NAME, null);
+        editor.putString(KEY_ACCESS_TOKEN, null);
+        editor.putString(KEY_USERNAME, null);
         editor.commit();
     }
 
 
     public String getUsername() {
-        return sharedPref.getString(API_USERNAME, null);
+        return sharedPref.getString(KEY_USERNAME, null);
     }
 
     public String getId() {
-        return sharedPref.getString(API_ID, null);
+        return sharedPref.getString(KEY_ID, null);
     }
 
     public String getName() {
-        return sharedPref.getString(API_NAME, null);
+        return sharedPref.getString(KEY_NAME, null);
     }
 
 
     public String getAccessToken() {
-        return sharedPref.getString(API_ACCESS_TOKEN, null);
+        return sharedPref.getString(KEY_ACCESS_TOKEN, null);
     }
 }
